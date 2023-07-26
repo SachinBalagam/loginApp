c015499e4c95e2c474f29c3d06e1147c09b3635c
SachinBalagam <balagamsachin337@gmail.com>
2023-07-26 17:30:59 +0000

    Final Commit

--- src/components/Home/index.js
+++ src/components/Home/index.js
@@ -1,1 +1,32 @@
 // Write your code here
+import {Component} from 'react'
+import Login from '../Login'
+import Logout from '../Logout'
+import Message from '../Message'
+import './index.css'
+
+class Home extends Component {
+  state = {isLogin: false}
+
+  onClickButton = () => {
+    this.setState(prevState => ({isLogin: !prevState.isLogin}))
+  }
+
+  render() {
+    const {isLogin} = this.state
+    return (
+      <div className="app-container">
+        <div className="home-container">
+          <Message isLoggedIn={isLogin} />
+          {isLogin ? (
+            <Logout logout={this.onClickButton} />
+          ) : (
+            <Login login={this.onClickButton} />
+          )}
+        </div>
+      </div>
+    )
+  }
+}
+
+export default Home
--- src/components/Login/index.js
+++ src/components/Login/index.js
@@ -1,1 +1,12 @@
-// Write your code here
+import './index.css'
+
+const Login = props => {
+  const {login} = props
+  return (
+    <button type="button" onClick={login} className="login-button">
+      Login
+    </button>
+  )
+}
+
+export default Login
